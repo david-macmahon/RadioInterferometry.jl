ed37a7b17836fb2ea8c3db60750e02f6bb4efba3
David MacMahon <davidm@astro.berkeley.edu>
2021-04-17 17:25:53 -0700

    Add some utility functions to ERFA if missing
    The utility functions `ae2hd`, `hd2ae`, and `hd2pa` are not included in
    ERFA 0.6.3.  The code for these three functions has been transcribed
    into Julia source files and they are included into the ERFA module at
    runtime if the loaded ERFA module does not already defined these
    functions.  This is a complete hack and should be removed once ERFA is
    updated to a more modern version that already includes these functions.

--- src/RadioInterferometry.jl
+++ src/RadioInterferometry.jl
@@ -35,6 +35,13 @@ import ERFA.DD2R
 import ERFA.DR2D
 import ERFA.CMPS
 
+function __init__()
+  # Add some utility functions to ERFA that are missing from eariler versions
+  isdefined(ERFA, :ae2hd) || Base.include(ERFA, joinpath(@__DIR__, "ae2hd.jl"))
+  isdefined(ERFA, :hd2ae) || Base.include(ERFA, joinpath(@__DIR__, "hd2ae.jl"))
+  isdefined(ERFA, :hd2pa) || Base.include(ERFA, joinpath(@__DIR__, "hd2pa.jl"))
+end
+
 import LinearAlgebra: mul!
 import Geodesy: ECEF, ENU
 
