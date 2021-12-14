1cbcaa593f1c5878d253a6cb7cb81e63a51535fb
David MacMahon <davidm@astro.berkeley.edu>
2021-12-14 04:43:05 +0000

    Remove "using Symbolics" from src/symbolics.jl
    RadioInterferometry uses Requires.jl to  add some Symbolics-aware
    features if/when the user uses both packagesd together, but
    RadioInterferometry does not depend on the Symbolics package so we have
    avoid "using Symbolics" as that causes Julia to complain that Symbolics
    is not in the project's dependencies.

--- src/symbolics.jl
+++ src/symbolics.jl
@@ -1,7 +1,5 @@
 # Extra functionality for when Symbolics is being used.
 
-using Symbolics
-
 export signclean
 
 """
@@ -11,11 +9,11 @@ A Symbolics rewriter for use with `simplify` that uses the trig identities:
 to rewrite sin/cos expressions without unary minus operations inside the
 arguments to `sin` snd `cos`.
 """
-signclean_rewriter = Rewriters.Prewalk(Rewriters.Chain([
-    @rule         sin(-1*(~x))         =>          -sin(~x)
-    @rule         cos(-1*(~x))         =>           cos(~x)
-    @rule (~w...)*sin(-1*(~x))*(~y...) => (~w...)*(-sin(~x))*(~y...)
-    @rule (~w...)*cos(-1*(~x))*(~y...) => (~w...)*( cos(~x))*(~y...)
+signclean_rewriter = Symbolics.Rewriters.Prewalk(Symbolics.Rewriters.Chain([
+    Symbolics.@rule         sin(-1*(~x))         =>          -sin(~x)
+    Symbolics.@rule         cos(-1*(~x))         =>           cos(~x)
+    Symbolics.@rule (~w...)*sin(-1*(~x))*(~y...) => (~w...)*(-sin(~x))*(~y...)
+    Symbolics.@rule (~w...)*cos(-1*(~x))*(~y...) => (~w...)*( cos(~x))*(~y...)
 ]))
 
 """
@@ -23,5 +21,5 @@ Cleanup any "negative" arguments in Symbolic `sin`/`cos` expressions in `r3`
 (typically rotation matrix) using trig identities.  Returns a new Array.
 """
 function signclean(r3=Array{Real})
-    simplify.(r3; rewriter=signclean_rewriter)
+    Symbolics.simplify.(r3; rewriter=signclean_rewriter)
 end
