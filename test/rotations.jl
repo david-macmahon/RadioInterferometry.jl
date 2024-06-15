ae599d7b2e91b123603dc07b4c2440aa42efb967
David MacMahon <davidm@astro.berkeley.edu>
2024-06-15 11:29:36 -0700

    Workaround to make weirdly broken tests unbroken

--- test/rotations.jl
+++ test/rotations.jl
@@ -23,13 +23,17 @@ r123_num      = rz(ψval)ry(θval)rx(ϕval)
 r123_val      = substitute.(r123     , [[ϕ=>ϕval, θ=>θval, ψ=>ψval]])
 r123_erfa_val = substitute.(r123_erfa, [[ϕ=>ϕval, θ=>θval, ψ=>ψval]])
 
+# There has to be an easier way to do this, right???  We convert to Float64s
+# because using `@test all(r123_val .≈ r123_erfa_val)` sometimes throws a weird
+# exception (for reasons unknown) even though other times it works fine.  If we
+# could understand/fix that latter problem then we wouldn't need to convert to
+# Float64s.
+r123_float = Symbolics.value.(parent(r123_val))
+r123_erfa_float = Symbolics.value.(parent(r123_erfa_val))
+
 @testset "rotations" begin
     @test all(r123_num .≈ r123_val)
-    # This sometimes passes and sometimes fails with:
-    # `MethodError: no method matching decompose(::Num)`
-    @test all(r123_val .≈ r123_erfa_val) skip=true
+    @test all(r123_float .≈ r123_erfa_float)
     @test all((r123_num * v) .≈ (r123_val * v))
-    # This sometimes passes and sometimes fails with:
-    # `MethodError: no method matching decompose(::Num)`
-    @test all((r123_val * v) .≈ (r123_erfa_val * v)) skip=true
+    @test all((r123_float * v) .≈ (r123_erfa_float * v))
 end
